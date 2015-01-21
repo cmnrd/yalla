3fbbf5fa305988c8ca9bf33f80814c79db0f8005
John Doe <christian.menard@gmx.de>
2015-01-21 23:45:46 +0100

    Added class IOMMPtr that allows for easy access of memory mapped registers

---
+++ include/yalla/iomm.hpp
@@ -0,0 +1,69 @@
+/**
+ * @brief  Structure for IOMM access
+ * @author Christian Menard
+ * @date   2015-01-21
+ */
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Christian Menard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <avrconstants.hpp>
+#include <inline.hpp>
+
+namespace yalla
+{
+
+/**
+ * A static pointer that provides read and write access to the I/O Memory Space.
+ *
+ * @tparam T       type that is used for read and write access
+ * @tparam addr    the address this pointer points to (absolute memory address)
+ * @tparam andMask a AND-mask applied to the value on each write
+ * @tparam orMask  a OR-mask applied to the value on each write (after andMask)
+ *
+ * TODO for types larger than 1 byte interrupts need to be disabled on each 
+ *      write and read access.
+ */
+template<typename T,
+         size_t   addr,
+         T        andMask = static_cast<T>(-1),
+         T        orMask  = static_cast<T>(0)>
+class IOMMPtr
+{
+private:
+	using PtrType = volatile T*;
+	static constexpr PtrType ptr = (PtrType) addr;
+public:
+	static INLINE T    read()     { return *ptr; }
+	static INLINE void write(T v) { *ptr = (v & andMask) | orMask; }
+
+	// sanity check
+	static_assert(addr >= AvrConstants::iommBase && 
+	              addr + sizeof(T) < AvrConstants::iommTop,
+	              "IOMMPtr points to region outside of I/O memory!");
+};
+
+} // namespace lunacy
---
+++ test/iomm/iomm_trace.c
@@ -0,0 +1,40 @@
+/**
+ * @brief  simavr trace settings for iomm test
+ * @author Christian Menard
+ * @date   2015-01-21
+ */
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Christian Menard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <avr_mcu_section.h>
+
+AVR_MCU(8000000, "atmega8");
+
+const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ =
+{
+	{ AVR_MCU_VCD_SYMBOL("PORTD"), .what = (void*)0x32},
+	{ AVR_MCU_VCD_SYMBOL("PORTC"), .what = (void*)0x35},
+	{ AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)0x38},
+};
