c9a0fb5960f4cd796c9eb6db0af34d9b49be4a19
John Doe <christian.menard@gmx.de>
2015-01-21 23:37:51 +0100

    Add basic header files
    - avrconstants.hpp defines constants defined by the AVR ISA
    - inline.hpp defines the INLINE macro
    - inttypes.hpp defines basic integer types

---
+++ include/yalla/avrconstants.hpp
@@ -0,0 +1,49 @@
+/**
+ * @brief  Definition of AVR ISA specific constants
+ * @author Christian Menard
+ * @date   2015-01-21
+ */
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Christian Menard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <inttypes.hpp>
+
+namespace yalla
+{
+
+/**
+ * A simple structure used to organize constants specified by the AVR ISA.
+ */
+struct AvrConstants
+{
+	/// base address of the memory region reserved for I/O Registers
+	static constexpr size_t iommBase = 0x20;
+	/// upper bound of the memory region reserved for I/O Registers
+	static constexpr size_t iommTop  = 0x5f;
+};
+
+} // namespace lunacy
---
+++ include/yalla/inttypes.hpp
@@ -0,0 +1,48 @@
+/**
+ * @brief  Basic integer type definitions
+ * @author Christian Menard
+ * @date   2015-01-21
+ */
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Christian Menard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+namespace yalla
+{
+
+using uint8_t  = unsigned char;
+using  int8_t  =   signed char;
+using uint16_t = unsigned short;
+using  int16_t =   signed short;
+using uint32_t = unsigned long;
+using  int32_t =   signed long;
+using uint64_t = unsigned long long;
+using  int64_t =   signed long long;
+
+using size_t   = unsigned short;
+using addr_t   = unsigned short;
+
+} // namespace lunacy
