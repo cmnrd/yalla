b1ec1615bd607758b4e88c87de20033e9964b4b2
Hannes Ellinger <hannes.ellinger@mailbox.tu-dresden.de>
2015-01-29 16:30:22 +0100

    Added some register definitions for Atmega8

--- include/yalla/device/atmega8/avr/io.hpp
+++ include/yalla/device/atmega8/avr/io.hpp
@@ -88,7 +88,87 @@ using SP2 = SREG::Bit2;
 using SP1 = SREG::Bit1;
 using SP0 = SREG::Bit0;
 
-// ... TODO
+using GICR = Register<0x5b,
+                     ReadWriteBit,  // INT1
+                     ReadWriteBit,  // INT0
+                     ReservedBit,   // -
+                     ReservedBit,   // -
+                     ReservedBit,   // -
+                     ReservedBit,   // -
+                     ReadWriteBit,  // IVSEL
+                     ReadWriteBit>; // IVCE
+
+using INT1  = GIRC::Bit7;
+using INT0  = GIRC::Bit6;
+using IVSEL = GIRC::Bit1;
+using IVCE  = GIRC::Bit0;
+
+using GIFR = Register<0x5A,
+                     StatusBitClearBy1,  // INTF1
+                     StatusBitClearBy1,  // INTF0
+                     ReservedBit,   // -
+                     ReservedBit,   // -
+                     ReservedBit,   // -
+                     ReservedBit,   // -
+                     ReservedBit,   // -
+                     ReservedBit>;  // -
+
+using INTF1 = GIFR::Bit7;
+using INTF0 = GIFR::Bit6;
+
+using TIMSK = Register<0x59,
+                     ReadWriteBit,  // OCIE2
+                     ReadWriteBit,  // TOIE2
+                     ReadWriteBit,  // TICIE1
+                     ReadWriteBit,  // OCIE1A
+                     ReadWriteBit,  // OCIE1B
+                     ReadWriteBit,  // TOIE1
+                     ReservedBit,   // -
+                     ReadWriteBit>; // TOIE0
+
+using OCIE2 = TIMSK::Bit7;
+using TOIE2 = TIMSK::Bit6;
+using TICIE1 = TIMSK::Bit5;
+using OCIE1A = TIMSK::Bit4;
+using OCIE1B = TIMSK::Bit3;
+using TOIE1 = TIMSK::Bit2;
+using TOIE0 = TIMSK::Bit0;
+
+using TIFR = Register<0x58,
+                     ReadWriteBit,  // OCF2
+                     ReadWriteBit,  // TOV2
+                     ReadWriteBit,  // ICF1
+                     ReadWriteBit,  // OCF1A
+                     ReadWriteBit,  // OCF1B
+                     ReadWriteBit,  // TOV1
+                     ReservedBit,   // -
+                     ReadWriteBit>; // TOV0
+
+using OCF2 = TIFR::Bit7;
+using TOV2 = TIFR::Bit6;
+using ICF1 = TIFR::Bit5;
+using OCF1A = TIFR::Bit4;
+using OCF1B = TIFR::Bit3;
+using TOV1 = TIFR::Bit2;
+using TOV0 = TIFR::Bit0;
+
+using SPMCR = Register<0x57,
+                     ReadWriteBit,  // SPMIE
+                     ReadWriteBit,  // RWWSB
+                     ReservedBit,   // -
+                     ReadWriteBit,  // RWWSRE
+                     ReadWriteBit,  // BLBSET
+                     ReadWriteBit,  // PGWRT
+                     ReadWriteBit,  // PGERS
+                     ReadWriteBit>; // SPMEN
+
+using SPMIE = SPMCR::Bit7;
+using RWWSB = SPMCR::Bit6;
+using RWWSRE = SPMCR::Bit4;
+using BLBSET = SPMCR::Bit3;
+using PGWRT = SPMCR::Bit2;
+using PGERS = SPMCR::Bit1;
+using SPMEN = SPMCR::Bit0;
 
 using TWCR = Register<0x56,
                       StatusBitClearBy1, // TWINT
@@ -107,6 +187,46 @@ using TWSTO = TWCR::Bit4;
 using TWWC  = TWCR::Bit3;
 using TWEN  = TWCR::Bit2;
 using TWIE  = TWCR::Bit0;
+
+using MCUCR = Register<0x55,
+                       ReadWriteBit, // SE
+                       ReadWriteBit, // SM2
+                       ReadWriteBit, // SM1
+                       ReadWriteBit, // SM0
+                       ReadWriteBit, // ISC11
+                       ReadWriteBit, // ISC10
+                       ReadWriteBit, // ISC01
+                       ReadWriteBit, // ISC00
+
+using SE = MCUCR::Bit7;
+using SM2 = MCUCR::Bit6;
+using SM1 = MCUCR::Bit5;
+using SM0 = MCUCR::Bit4;
+using BIT3 = MCUCR::Bit3;
+using BIT2 = MCUCR::Bit2;
+using ISC01 = MCUCR::Bit1;
+using ISC00 = MCUCR::Bit0;
+
+using MCUCSR = Register<0x54,
+                        ReservedBit,  // -
+                        ReservedBit,  // -
+                        ReservedBit,  // -
+                        ReservedBit,  // -
+                        ReadWriteBit, // WDRF
+                        StatusBitClearBy0, // BORF
+                        StatusBitClearBy0, // EXTRF
+                        StatusBitClearBy0, // PORF
+
+// RESERVED = MCUCSR::Bit7;
+// RESERVED = MCUCSR::Bit6;
+// RESERVED = MCUCSR::Bit5;
+// RESERVED = MCUCSR::Bit4;
+using WDRF = MCUCSR::Bit3;
+using BORF = MCUCSR::Bit2;
+using EXTRF = MCUCSR::Bit1;
+using PORF = MCUCSR::Bit0;
+
+
 
 // ... TODO
 
